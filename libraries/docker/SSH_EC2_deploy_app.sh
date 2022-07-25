49ae0b662d2d7da2559f77fc279f15a55c05f5d8
Evan Floyd <evan.floyd@idevops.io>
2022-07-24 23:54:28 -0700

    modified ssh command

--- libraries/docker/SSH_EC2_deploy_app.sh
+++ libraries/docker/SSH_EC2_deploy_app.sh
@@ -1,15 +1,20 @@
 #!/bin/sh -l
 set -eu
 
+ssh_cmd="$(cat <<-EOF
+    whoami 
+    if [ ! -d "evans-apps" ]; then 
+    sudo git clone https://github.com/evanfloyd/evans-apps.git 
+    fi 
+    cd evans-apps/docker/${image_name} 
+    sudo docker-compose up 
+EOF
+)"
+
 sudo touch /private_key.pem
 KEY='${KEY}'
 TEMP_SSH_PRIVATE_KEY_FILE='../private_key.pem'
 printf "%s" "$KEY" >$TEMP_SSH_PRIVATE_KEY_FILE
 chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
-ssh -i $TEMP_SSH_PRIVATE_KEY_FILE -o StrictHostKeyChecking=no ${EC2_INSTANCE_USERNAME}@${INSTANCE_DNS_NAME} \
-whoami \
-if [ ! -d "evans-apps" ]; then \
-  sudo git clone https://github.com/evanfloyd/evans-apps.git \
-fi \
-cd evans-apps/docker/${image_name} \
-sudo docker-compose up \
+ssh -i $TEMP_SSH_PRIVATE_KEY_FILE -o StrictHostKeyChecking=no ${EC2_INSTANCE_USERNAME}@${INSTANCE_DNS_NAME} "$ssh_cmd"
+
