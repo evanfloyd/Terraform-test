1a8bf44d404b863c1bdd062e11a329b03a71424c
Evan Floyd <evan.floyd@idevops.io>
2022-07-25 00:07:37 -0700

    modified ssh command

--- libraries/docker/SSH_EC2_deploy_app.sh
+++ libraries/docker/SSH_EC2_deploy_app.sh
@@ -15,6 +15,6 @@ sudo touch private_key.pem
 KEY='${KEY}'
 TEMP_SSH_PRIVATE_KEY_FILE='private_key.pem'
 sudo bash -c "printf "%s" "$KEY" >$TEMP_SSH_PRIVATE_KEY_FILE"
-sudo bash -c "chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE"
+chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 ssh -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE ${EC2_INSTANCE_USERNAME}@${INSTANCE_DNS_NAME} "$ssh_cmd"
 
